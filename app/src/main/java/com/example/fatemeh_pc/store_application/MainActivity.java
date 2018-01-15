eaea45698b58d8da4002a374f53619e786f35d60
khalifeyifatemeh <khalifeyifatemeh@gmail.com>
2018-01-15 04:41:38 -0800

    @ activity_about.xml and activity_soal.xml edited

--- app/src/main/java/com/example/fatemeh_pc/store_application/MainActivity.java
+++ app/src/main/java/com/example/fatemeh_pc/store_application/MainActivity.java
@@ -4,14 +4,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View ;
+import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.app.Activity;
 import android.widget.SearchView;
 import android.widget.TextView;
+import android.content.Intent;
+import android.widget.Toast;
+import android.widget.ImageView;
+
+import android.app.ActionBar;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.RelativeLayout;
+import android.view.Gravity;
+import android.graphics.Color;
+import android.widget.LinearLayout;
+
+
+
+
+
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private static ImageView imgview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,39 +49,57 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         int id = item.getItemId();
         if (id == R.id.id_vorud) {
+            Intent intent = new Intent(this, vorud_activity.class);
+            this.startActivity(intent);
             return true;
         }
         if (id == R.id.id_home) {
+            Intent intent = new Intent(this, MainActivity.class);
+            this.startActivity(intent);
             return true;
         }
         if (id == R.id.id_about) {
+            Intent intent = new Intent(this, about_activity.class);
+            this.startActivity(intent);
             return true;
         }
         if (id == R.id.id_category) {
+            Intent intent = new Intent(this, category_activity.class);
+            this.startActivity(intent);
             return true;
         }
         if (id == R.id.id_sabad) {
+            Intent intent = new Intent(this, sabad_activity.class);
+            this.startActivity(intent);
             return true;
         }
         if (id == R.id.id_vizheh) {
+            Intent intent = new Intent(this, vizheh_activity.class);
+            this.startActivity(intent);
             return true;
         }
         if (id == R.id.id_soal) {
+            Intent intent = new Intent(this, soal_activity.class);
+            this.startActivity(intent);
             return true;
         }
         if (id == R.id.id_search) {
+            Intent intent = new Intent(this, search_activity.class);
+            this.startActivity(intent);
             return true;
         }
         if (id == R.id.id_sabad2) {
+            Intent intent = new Intent(this, sabad_activity.class);
+            this.startActivity(intent);
             return true;
         }
 
-        hi siavash
         return super.onOptionsItemSelected(item);
-    }
 
+    }
 
 
 }
---
+++ app/src/main/java/com/example/fatemeh_pc/store_application/sabad_activity.java
@@ -0,0 +1,13 @@
+package com.example.fatemeh_pc.store_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sabad_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sabad_activity);
+    }
+}
