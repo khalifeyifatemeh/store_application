fa70aa89232f035427fd729ca9bf2e02909bbc6e
khalifeyifatemeh <khalifeyifatemeh@gmail.com>
2018-01-18 11:35:45 -0800

    @ edit databasehelper.java create tabele and some other basic stuffs

--- app/src/main/java/com/example/fatemeh_pc/store_application/DatabaseHelper.java
+++ app/src/main/java/com/example/fatemeh_pc/store_application/DatabaseHelper.java
@@ -5,33 +5,70 @@ import android.database.sqlite.SQLiteOpenHelper;
 import static nl.qbusict.cupboard.CupboardFactory.cupboard;
 
 public class DatabaseHelper extends SQLiteOpenHelper{
-    private static final String DATABASE_NAME = "moblplus.db";
+
+    // Logcat tag
+    private static final String LOG = "DatabaseHelper";
+
+    // Database Version
     private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "moblplus.db";
+
+    // Table Names
+    private static final String TABLE_customer  = "customer";
+    private static final String TABLE_furniture = "furniture";
+    private static final String TABLE_invoice   = "invoice";
+    private static final String TABLE_cfi       = "cfi";
+
+    // Table Create Statements
+    // customer table create statement
+    private static final String CREATE_TABLE_customer = "CREATE TABLE " +
+            TABLE_customer + "( cid TEXT PRIMARY KEY, cname TEXT, clastname TEXT," +
+            " cemail TEXT, cphone TEXT,cpassword TEXT, cpostalcode TEXT, caddress TEXT, " +
+            " ctype TEXT, cshopcount INTEGER)";
+
+    // furniture table create statement
+    private static final String CREATE_TABLE_furniture = "CREATE TABLE " +
+            TABLE_furniture + "( fid TEXT PRIMARY KEY, fname TEXT, ftype TEXT," +
+            " fprice TEXT, fcolor TEXT, ffabric TEXT, fwood TEXT, fkosan TEXT, " +
+            " fexist TEXT, fimage TEXT)";
+
+    // invoice table create statement
+    private static final String CREATE_TABLE_invoice = "CREATE TABLE " +
+            TABLE_invoice + "( itransactionid INTEGER PRIMARY KEY, itotalprice INTEGER," +
+            " itransportprice INTEGER, ioffprice INTEGER, idate DATETIME)";
+
+    // invoice table create statement
+    private static final String CREATE_TABLE_cfi = "CREATE TABLE " +
+            TABLE_cfi + "(cid TEXT, fid TEXT, itransactionid INTEGER," +
+            " PRIMARY KEY (cid, fid, itransactionid))";
+
+
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    static {
-        // register our models
-        cupboard().register(Bunny.class);
-    }
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // this will ensure that all tables are created
-        cupboard().withDatabase(db).createTables();
-        // add indexes and other database tweaks in this method if you want
-
+        // creating required tables
+        db.execSQL(CREATE_TABLE_customer);
+        db.execSQL(CREATE_TABLE_furniture);
+        db.execSQL(CREATE_TABLE_invoice);
+        db.execSQL(CREATE_TABLE_cfi);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // this will upgrade tables, adding columns and new tables.
-        // Note that existing columns will not be converted
-        cupboard().withDatabase(db).upgradeTables();
-        // do migration work if you have an alteration to make to your schema here
+        // on upgrade drop older tables
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_customer);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_furniture);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_invoice);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_cfi);
 
+        // create new tables
+        onCreate(db);
     }
 
 }
