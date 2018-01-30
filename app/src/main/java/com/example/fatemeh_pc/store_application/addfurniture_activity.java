29666a6fee39c6469a75cb95fa1171db638fb8a2
khalifeyifatemeh <khalifeyifatemeh@gmail.com>
2018-01-30 07:02:28 -0800

    spinner

--- app/src/main/java/com/example/fatemeh_pc/store_application/addfurniture_activity.java
+++ app/src/main/java/com/example/fatemeh_pc/store_application/addfurniture_activity.java
@@ -20,84 +20,95 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 public class addfurniture_activity extends AppCompatActivity {
-
-
-    private static int IMG_RESULT = 1;
-    ImageView imageViewLoad;
-    Button LoadImage;
     Intent intent;
     DatabaseHelper db;
-    ImageView imageview;
     EditText editfimage;
+    String[] CodesOfFurniture = { "1","2","3","4","5"};
+
+    Spinner sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addfurniture_activity);
-
-        imageViewLoad = (ImageView) findViewById(R.id.id_imageview);
-        LoadImage = (Button) findViewById(R.id.id_btn_addimage);
         db = new DatabaseHelper(getApplicationContext());
 
-//        //spinners code
-//        Spinner sp = (Spinner) findViewById(R.id.id_spinner_furniture);
+
+        final EditText editfid = (EditText) findViewById(R.id.id_editText_fid);
+        final EditText editfname = (EditText) findViewById(R.id.id_editText_fname);
+        final EditText editftype = (EditText) findViewById(R.id.id_editText_ftype);
+        final EditText editfpirce = (EditText) findViewById(R.id.id_editText_fprice);
+        final EditText editfcolor = (EditText) findViewById(R.id.id_editText_fcolor);
+        final EditText editffabric = (EditText) findViewById(R.id.id_editText_ffabric);
+        final EditText editfwood = (EditText) findViewById(R.id.id_editText_fwood);
+        final EditText editfcount = (EditText) findViewById(R.id.id_editText_fcount);
+        final EditText editfkosan = (EditText) findViewById(R.id.id_editText_fkosan);
+        final EditText editfexist = (EditText) findViewById(R.id.id_editText_fexist);
+        editfimage = (EditText) findViewById(R.id.id_editText_image);
+        Button btnaddfurniture = (Button) findViewById(R.id.id_btn_addfurniture);
+        Button btneditfurniture = (Button) findViewById(R.id.id_btn_editfurniture);
+        Button btndeletfurniture = (Button) findViewById(R.id.id_btn_deletefurniture);
+
+
+//        List<addfurniture> af = db.getAllFurnitures();
+//        if (af.size() > 0) {
+//            for (addfurniture furniture : af) {
+//                String s = furniture.getFid() + " " + furniture.getFname();
+//                CodesOfFurniture.add(s);
 //
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,  android.R.layout.simple_dropdown_item_1line,furnitur_code);
-//        adapter.setDropDownViewResource(android.R.layout.
-//                simple_spinner_dropdown_item);
-//
-//        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> arg0, View arg1,int position, long id) {
-//             Toast.makeText(addfurniture_activity.this, "Entekhab shoma: " + furnitur_code[position], Toast.LENGTH_SHORT).show();
 //            }
+//        }
+        sp = (Spinner) findViewById(R.id.id_spinner_furniture);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,  android.R.layout.simple_dropdown_item_1line,CodesOfFurniture);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            //5
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1,int position, long id) {
 
-//            @Override
-//            public void onNothingSelected(AdapterView<?> arg0) {
-//            }
-//        });
+                Toast.makeText(addfurniture_activity.this,"U Choosed : " + CodesOfFurniture[position],Toast.LENGTH_SHORT).show();
+                String code=CodesOfFurniture[position];
+                int code2=(int)Integer.parseInt(code);
+                addfurniture f=new addfurniture();
+                f=db.getfurniture(code2);
+                editfid.setText(code);
+                editfname.setText(f.fname);
+                editftype.setText(f.ftype);
+                editfpirce.setText(f.fprice);
+                editfcolor.setText(f.fcolor);
+                editffabric.setText(f.ffabric);
+                editfwood.setText(f.fwood);
+                editfkosan.setText(f.fkosan);
+                editfcount.setText(f.fcount);
+                editfexist.setText(f.fexist);
+                editfimage.setText(f.fimage);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
 
-        final EditText editfid         = (EditText) findViewById(R.id.id_editText_fid);
-        final EditText editfname       = (EditText) findViewById(R.id.id_editText_fname);
-        final EditText editftype       = (EditText) findViewById(R.id.id_editText_ftype);
-        final EditText editfpirce      = (EditText) findViewById(R.id.id_editText_fprice);
-        final EditText editfcolor      = (EditText) findViewById(R.id.id_editText_fcolor);
-        final EditText editffabric     = (EditText) findViewById(R.id.id_editText_ffabric);
-        final EditText editfwood       = (EditText) findViewById(R.id.id_editText_fwood);
-        final EditText editfcount      = (EditText) findViewById(R.id.id_editText_fcount);
-        final EditText editfkosan      = (EditText) findViewById(R.id.id_editText_fkosan);
-        final EditText editfexist      = (EditText) findViewById(R.id.id_editText_fexist);
-        editfimage      = (EditText) findViewById(R.id.id_edittex_image);
-        Button btnaddimage              =(Button)   findViewById(R.id.id_btn_addimage);
-        Button btnaddfurniture              =(Button)   findViewById(R.id.id_btn_addfurniture);
-        Button btneditfurniture         =(Button)   findViewById(R.id.id_btn_editfurniture);
-        Button btndeletfurniture         =(Button)  findViewById(R.id.id_btn_deletefurniture);
-
-
-        btnaddimage.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                /*****
-                 * insert image folder address to database
-                 *****/
             }
         });
 
-        btnaddfurniture.setOnClickListener(new View.OnClickListener(){
-            public void onClick (View view){
+
+
+        btnaddfurniture.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
                 /*****
                  * insert furniture to database
                  *****/
                 try {
                     addfurniture fr = new addfurniture();
-                    int fid=(int) Integer.parseInt(editfid.getText().toString());
+                    int fid = (int) Integer.parseInt(editfid.getText().toString());
                     fr.setFid(fid);
                     fr.setFname(editfname.getText().toString());
                     fr.setFtype(editftype.getText().toString());
@@ -120,41 +131,58 @@ public class addfurniture_activity extends AppCompatActivity {
             }
         });
 
-//        btneditfurniture.setOnClickListener(new View.OnClickListener(){
-//            public void onClick (View arg0){
-//                /*****
-//                 * update furniture in database
-//                 *****/
-//            }
-//        });
-        Button btntest           = (Button) findViewById(R.id.id_btn_editfurniture);
-        //Button btn_furniture      = (Button) findViewById(R.id.btn_furniture);
-        //Button btn_invoice        = (Button) findViewById(R.id.btn_invoice);
-        //Button btn_ufi            = (Button) findViewById(R.id.btn_ufi);
-        //final TextView viewtest     = (TextView) findViewById(R.id.textview_test);
-        db = new DatabaseHelper(getApplicationContext());
-
-        btntest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                List<addfurniture> allfurnitures = db.getAllFurnitures();
-                if (allfurnitures.size() > 0) {
-                    for (addfurniture furniture : allfurnitures) {
-                        String s = furniture.getFid() + " " + furniture.getFname() + " " + furniture.getFcolor();
-                        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
-
-                    }
-                }
+        btneditfurniture.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View arg0){
+                /*****
+                 * update furniture in database
+                 *****/
             }
         });
-
-        db.closeDB();
-
-        btndeletfurniture.setOnClickListener(new View.OnClickListener(){
-            public void onClick (View arg0){
+//        Button btntest = (Button) findViewById(R.id.id_btn_editfurniture);
+//
+//        db = new DatabaseHelper(getApplicationContext());
+//
+//        btntest.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                List<addfurniture> allfurnitures = db.getAllFurnitures();
+//                if (allfurnitures.size() > 0) {
+//                    for (addfurniture furniture : allfurnitures) {
+//                        String s = furniture.getFid() + " " + furniture.getFname() + " " + furniture.getFcolor();
+//                        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+//
+//                    }
+//                }
+//            }
+//        });
+///////////////////////////////DELETE////////////////////////////////////
+        btndeletfurniture.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
                 /*****
                  * insert furniture to database
                  *****/
+//                try {
+//                    addfurniture fr = new addfurniture();
+//                    int fid=(int) Integer.parseInt(editfid.getText().toString());
+//                    fr.setFid(fid);
+//                    fr.setFname(editfname.getText().toString());
+//                    fr.setFtype(editftype.getText().toString());
+//                    fr.setFprice(editfpirce.getText().toString());
+//                    fr.setFcolor(editfcolor.getText().toString());
+//                    fr.setFwood(editfwood.getText().toString());
+//                    fr.setFfabric(editffabric.getText().toString());
+//                    fr.setFcount(editfcount.getText().toString());
+//                    fr.setFkosan(editfkosan.getText().toString());
+//                    fr.setFexist(editfexist.getText().toString());
+//                    fr.setFimage(editfimage.getText().toString());
+//
+//                    long result = db.createFurniture(fr);
+//
+//                    Toast.makeText(getApplicationContext(), result + "عملیات موفق", Toast.LENGTH_LONG).show();
+//                } catch (SQLException e) {
+//                    Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+//                }
+
             }
         });
         db.closeDB();
